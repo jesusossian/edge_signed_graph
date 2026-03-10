535b10bea13efd69d9172b38756d43973322b0fa
Jesus Ossian <jesusossian@gmail.com>
2026-03-10 16:07:02 -0300

    updated

--- run_edge_signed_random_py.sh
+++ run_edge_signed_random_py.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-method="lp"
+method="mip"
 
 for n in 60
 do
-    for k in 3 #2 3 4 5 
+    for k in 2 3 4 5 
     do
         for pos in 30
         do
@@ -14,7 +14,7 @@ do
                 do
                     for id in 1 2 3 4 5
                     do
-                        python3 src/edge_signed.py ${method} random_n${n}_k${k}_pos${pos}_neg${neg}_err${err}_${id}.g >> /home/jossian/Downloads/develop/report/signed_graphs/out_${method}_random_n${n}_k${k}_pos${pos}_neg${neg}_err${err}_${id}.txt
+                        python3 src/edge1_signed.py ${method} random_n${n}_k${k}_pos${pos}_neg${neg}_err${err}_${id}.g >> /home/jossian/Downloads/develop/report/signed_graphs/out_${method}_random_n${n}_k${k}_pos${pos}_neg${neg}_err${err}_${id}.txt
                     done
                 done
             done
